c125cef87c4720b54d22dc8974b026b22c954577
user.email <caped.20211p2ads0169@aluno.ifpi.edu.br>
2023-06-19 17:05:26 -0300

    add o scanner (ainda com defeito).

---
+++ lib/scanner.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class QRCodePage extends StatefulWidget {
+  const QRCodePage({Key? key}) : super(key: key);
+
+  @override
+  State<QRCodePage> createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRCodePage> {
+  String ticket = '';
+  List<String> tickets = [];
+
+  readQRCode() async {
+    String code = await FlutterBarcodeScanner.scanBarcode(
+      "#FFFFFF",
+      "Cancelar",
+      false,
+      ScanMode.QR,
+    );
+    setState(() => ticket = code != '-1' ? code : 'Não validado');
+
+    // Stream<dynamic>? reader = FlutterBarcodeScanner.getBarcodeStreamReceiver(
+    //   "#FFFFFF",
+    //   "Cancelar",
+    //   false,
+    //   ScanMode.QR,
+    // );
+    // if (reader != null)
+    //   reader.listen((code) {
+    //     setState(() {
+    //       if (!tickets.contains(code.toString()) && code != '-1')
+    //         tickets.add(code.toString());
+    //     });
+    //   });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return readQRCode();
+  }
+}
